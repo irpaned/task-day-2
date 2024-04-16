e9158d7dc4a48ed3f3011ebe449a1106f047f0bd
irpaned <muhammadirfan2823@gmail.com>
2024-04-16 14:24:39 +0800

    menambahkan function

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,7 +1,7 @@
-function sumbitHandler () {
-    let name = document.getElementById("nama").value;
+function submitHandler() {
+    let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
-    let phonenumber = document.getElementById("phonenumber").value;
+    let phoneNumber = document.getElementById("phonenumber").value;
     let subject = document.getElementById("subject").value;
     let message = document.getElementById("message").value;
 
@@ -9,22 +9,23 @@ function sumbitHandler () {
         return alert ("please entered your Name!")
     } else if (email == "") {
         return alert ("please entered your Email!")
-    } else if (phonenumber == "") {
+    } else if (phoneNumber == "") {
         return alert ("please entered your Phone Number!")
     } else if (subject == "") {
         return alert ("please entered your Subject!")
     } else if (message == "") {
-        return alert ("please entered your message!")
+        return alert ("please entered your Message!")
     }
 
-    // menampung data
-    const data = {name, email, phonenumber, subject, message}
+    const data = {
+        name, email, phoneNumber, subject, message
+    }
 
     const yourEmail = "muhammadirfan2823@gmail.com"
 
-    let a = document.createElement ("a")
-    a.href = `https://mail.google.com/mail/view-cm&fs-1&to=${yourEmail}&su-${subject}&body-${message}`
-    a.click():
+    let a = document.createElement("a")
+    a.href = `https://mail.google.com/mail?view=cm&fs=1&to=${yourEmail}&su=${subject}&body=${message}`
+    a.click();
 
     console.log(data)
 }
